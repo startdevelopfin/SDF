1c207841c249245ac0339feb7e8f00a6c17a882d
startdevelopfin <danieljaupont@gmail.com>
2025-03-09 00:33:38 -0500

    Adds style to posts

--- Sources/ContentLayouts/About.swift
+++ Sources/ContentLayouts/About.swift
@@ -21,7 +21,7 @@ struct About: StaticLayout {
         Card {
             Text("What I Do 🛠️")
                 .font(.title2)
-                .margin(.bottom, .large)
+                .margin(.bottom, .medium)
             
             Text("I create intuitive iOS applications that solve real problems while maintaining clean, maintainable codebases. When I'm not building software, I'm sharing knowledge through:")
             
@@ -38,25 +38,28 @@ struct About: StaticLayout {
                 
         }
         .margin(.bottom, .large)
+        .background(.steelBlue.opacity(0.35))
 
         Card {
             Text("My Approach 🎯")
                 .font(.title2)
-                .margin(.bottom, .large)
+                .margin(.bottom, .medium)
             
             Text("I believe great software emerges from the intersection of technical excellence and intuitive design. As an independent developer, I have the freedom to pursue projects that align with this philosophy, whether client work or personal endeavors.")
         }
         .margin(.bottom, .large)
+        .background(.mediumTurquoise.opacity(0.35))
         
         Card {
             Text("Let's Connect 🌍")
                 .font(.title3)
-                .margin(.bottom, .large)
+                .margin(.bottom, .medium)
             
             Text("Whether you're looking for development expertise, considering a mentorship opportunity, or interested in having me speak at your event, I welcome the conversation. While this blog offers insights into my technical perspective, you can learn more about my professional background on LinkedIn or follow my coding journey on GitHub.")
-            SocialFooter()
-            
         }
-        .margin(.bottom, .medium)
+        .margin(.bottom, .large)
+        .background(.oliveDrab.opacity(0.35))
+
+        SocialFooter()
     }
 }
